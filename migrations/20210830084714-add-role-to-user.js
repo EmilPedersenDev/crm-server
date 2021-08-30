89baead3baab6c0bed15ce263ae6f03e7d94f72d
Emil Pedersen <emil.pedersen@stretch.se>
2021-08-30 10:56:49 +0200

    adding role migration

---
+++ migrations/20210830084714-add-role-to-user.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Users", "role", Sequelize.STRING);
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("Users", "role");
+
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
